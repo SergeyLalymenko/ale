c61e3558338c1ae004e9cb71e51b843b55238a53
SergeyLalymenko <Lalymenko.Sergey153@gmail.com>
2025-03-28 17:32:54 +0200

    update params render in thank you page

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,5 +1,6 @@
-'use client';
+'use client'
 
+import { Suspense } from 'react';
 import Link from 'next/link';
 import { useSearchParams  } from 'next/navigation';
 import { Routes } from '@/const/routes';
@@ -9,35 +10,48 @@ interface IParamsTranslations {
     [key: string]: string;
 }
 
-function ThankYou() {
-    const searchParams: URLSearchParams = useSearchParams();
-    const params: IParamsTranslations = Object.fromEntries(searchParams.entries());
-    const paramsTranslations: IParamsTranslations = {
-        name: 'Name',
-        email: 'Email',
-        assignment_description: 'Assignment Description',
-        github_repo_url: 'GitHub Repository URL',
-        candidate_level: 'Candidate Level'
-    };
+const paramsTranslations: IParamsTranslations = {
+    name: 'Name',
+    email: 'Email',
+    assignment_description: 'Assignment Description',
+    github_repo_url: 'GitHub Repository URL',
+    candidate_level: 'Candidate Level'
+};
+
+function ThankYouInner() {
+    const searchParams = useSearchParams();
+
+    function renderParams() {
+        return Array.from(searchParams.entries()).map(([name, value]: string[], i) => (
+            <p key={i} className="mt-2">
+                {paramsTranslations[name]}: {value}
+            </p>
+        ));
+    }
 
     return (
         <div className="flex flex-col items-center container mx-auto px-4 py-5">
             <p className="mt-8 mb-4 text-5xl">
                 Thank you for submitting your assignment!
             </p>
-            {
-                Object.entries(params).map(([name, value]: string[], i) => (
-                    <p key={i} className="mt-2">
-                        {paramsTranslations[name]}: {value}
-                    </p>
-                ))
-            }
+            <Suspense fallback={<div>Loading...</div>}>
+                {renderParams()}
+            </Suspense>
             <Link href={Routes.HOME} className="mt-4">
                 <Button type="button">
                     Back
                 </Button>
             </Link>
         </div>
+    )
+}
+
+function ThankYou() {
+
+    return (
+        <Suspense>
+            <ThankYouInner />
+        </Suspense>
     )
 }
 
